32ea52ade6c552278eed2f9609e17e2dcaa2196a
Juna Shrestha <name@gmail.com>
2022-07-04 13:40:59 +0545

    sequnece

---
+++ KEY SEQUNCE/index.js
@@ -0,0 +1,21 @@
+// let array = [];
+// let keybox = document.querySelector('#keybox');
+// let log = document.querySelector('#log');
+
+// window.addEventListener('keyup', (e) => {
+//     if (e.key == "Backspace") {
+//         array = array.slice(0, array.length - 1);
+//         log.innerHTML = '[' + array + ']';
+//     }
+//     else {
+//         keybox.innerHTML = e.key;
+//         array.push(e.key);
+//         log.innerHTML = '[' + array + ']';
+//         console.log(array);
+//     }
+//     if (array.toString() == ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"]) {
+//         console.log('hello');
+//         keybox.innerHTML = keybox.innerHTML = "ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a", "Enter"
+
+//     }
+// })
